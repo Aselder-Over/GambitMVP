3d9c58d55cd498dd5ca02e16eda2e4903eeeaa91
Aselder <asover98@gmail.com>
2020-09-02 15:30:09 +0300

    Initial Commit

---
+++ GambitMVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GambitMVP
+//
+//  Created by Асельдер on 02.09.2020.
+//  Copyright © 2020 Асельдер. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
